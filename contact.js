1a98e1eb44e0e328df5d0aaa7d5d27456576a52c
Vishva <adharsh.scipy@gmail.com>
2024-09-30 17:58:30 +0530

    new version

--- contact.js
+++ contact.js
@@ -2,14 +2,14 @@ function validateForm() {
     document.getElementById("usernameError").innerText = "";
     document.getElementById("emailError").innerText = "";
 
-    var username = document.getElementById("username").value;
-    var email = document.getElementById("email").value;
+var username = document.getElementById("username").value;
+var email = document.getElementById("email").value;
 
-    if (username.length < 3) {
-        document.getElementById("usernameError").innerText = "username must be at least 3 characters";
-        return false;
-    }
-    return true;
+if (username.length < 3) {
+    document.getElementById("usernameError").innerText = "username must be at least 3 characters";
+    return false;
+}
+return true;
 
 
 }
